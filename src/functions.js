3e9cb7fe16b7335a73efde2a1bb1353cdcff05e0
Michael Bailey <Michael.Bailey@LearningCurveGroup.co.uk>
2021-08-25 16:16:25 +0100

    API Changes

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
 function App() {
     const handleSubmit = event => {
       event.preventDefault();
       alert ('You have successfully submitted this bug')
     }
+
+function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
+}
+
+export default App;
 }
